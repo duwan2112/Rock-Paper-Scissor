3b887dde0b3ad28954d562401536a0b7927cfcad
Duwan Peña <duwanp2112@gmail.com>
2020-07-05 14:00:53 -0430

    Updates

---
+++ precache-manifest.d8d96ab0edeb2f6b2d3b38a056abb907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00aafa557e954ec586d35932bce2492",
+    "url": "/Rock-Paper-Scissor/index.html"
+  },
+  {
+    "revision": "20fa38f7d376c1905365",
+    "url": "/Rock-Paper-Scissor/static/js/2.2dda319e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Rock-Paper-Scissor/static/js/2.2dda319e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505bb88dde6e98b322d7",
+    "url": "/Rock-Paper-Scissor/static/js/main.b2a4603b.chunk.js"
+  },
+  {
+    "revision": "e23e2dd2fac5b0765a06",
+    "url": "/Rock-Paper-Scissor/static/js/runtime-main.243a3839.js"
+  }
+]);
